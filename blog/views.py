25b06130603ce16c422616b5f90e19d77dcafff9
Tommi Tuomisto <ttu@kapsi.fi>
2023-01-12 22:21:29 +0200

    Part5

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'Tommi',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 7, 2023'
-    },
-    {
-        'author': 'Aija',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'January 8, 2023'
-    } 
-]
 
 def home(request):
     context = {
-            'posts': posts,
-            'title': 'Home'
+            'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
